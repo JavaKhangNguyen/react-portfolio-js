56a52eeefae8f950d936b167e6cb25d4c8b73b7d
Nguyễn Phúc Khang <khang2073@gmail.com>
2024-10-02 18:04:45 +0700

    Update responsive design

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,10 @@ module.exports = {
       screens: {
         xs: "320px",
         sm: "375px",
-        sml: "500px",
-        md: "667px",
-        mdl: "768px",
-        lg: "960px",
-        lgl: "1024px",
-        xl: "1280px",
+        md: "768px",
+        xl: "1024px",
+        lgl: "1280px",
+        xl: "1536px",
       },
       fontFamily: {
         bodyfont: ["Poppins", "sans-serif"],
