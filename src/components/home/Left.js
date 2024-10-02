a76982efabd46a0875aa72a214886733d8fad6f2
Nguyễn Phúc Khang <khang2073@gmail.com>
2024-10-02 21:26:23 +0700

    Update for mobile optimization

--- src/components/home/Left.js
+++ src/components/home/Left.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { useTypewriter, Cursor } from "react-simple-typewriter"
-import bannerImg from "../../assets/CV.jpg";
+import bannerImgJpg from "../../assets/CV.jpg";
+import bannerImgWebp from "../../assets/CV.webp";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faFileArrowDown, faPaperPlane} from "@fortawesome/free-solid-svg-icons"
 import { faFacebookF, faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons' 
-import { LazyLoadImage } from 'react-lazy-load-image-component'
 
 const Left = ({ handleContactClick }) => {
   const [text] = useTypewriter({
@@ -17,11 +17,16 @@ const Left = ({ handleContactClick }) => {
   return (
     <div className="w-full lgl:w-5/12 h-full bg-bodyColor rounded-2xl shadow-testShwdow z-10">
       <div className="w-full h-3/5">
-        <LazyLoadImage
-          className="w-full h-full object-contain md:object-scale-down rounded-2xl"
-          src={bannerImg}
-          alt="bannerImage"
-        />
+        <picture>
+          <source srcSet={bannerImgWebp} type="image/webp" />
+          <source srcSet={bannerImgJpg} type="image/jpeg" />
+          <img
+            className="w-full h-full object-contain md:object-scale-down rounded-2xl"
+            src={bannerImgJpg} // Fallback for browsers that don't support <picture>
+            loading="priority"
+            alt="bannerImage"
+          />
+        </picture>
       </div>
       <div className="w-full h-2/5 flex flex-col justify-between border-t-0 rounded-bl-xl rounded-br-xl">
         <div className="flex flex-col items-center gap-2 py-10">
