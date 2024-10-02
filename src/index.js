f4388b4b09d8c9b59612593178a1d81ddd9dcc84
Nguyễn Phúc Khang <khang2073@gmail.com>
2024-10-02 22:01:15 +0700

    Adjust deployment

--- src/index.js
+++ src/index.js
@@ -2,8 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { SpeedInsights } from '@vercel/speed-insights/react'
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
-<SpeedInsights />
+root.render(
+  <>
+    <App />
+    <SpeedInsights />
+  </>
+);
